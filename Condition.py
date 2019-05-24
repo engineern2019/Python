636a6dd6403bef94bd18d7befacb81529411d99d
engineern2019 <alidawood15@gmail.com>
2019-05-24 14:41:16 +0100

    flowchart_if_else

--- Condition.py
+++ Condition.py
@@ -1,7 +1,17 @@
-T=input("Please enter temprature = ")
-temp=int(T)
-if temp>40:
-	print(" it is hot ")
-if temp<40:
-	print(" it is cold ")
-print("Drink Some Lucazade")
+no=int(input("Please enter number = "))
+if no>1000:
+	print("B")
+	if no>2000:
+		print("C")
+	else:
+		print("F")
+	print("2")
+else:
+	if no<1000:
+		print("A")
+		if no>500:
+			print("D")
+		else:
+			print("E")
+	print("1")
+print("3")
