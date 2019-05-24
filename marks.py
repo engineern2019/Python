2f608030dab569736f77914b61a6485d1226043f
engineern2019 <alidawood15@gmail.com>
2019-05-24 10:16:23 +0100

    Day1_python

---
+++ marks.py
@@ -0,0 +1,14 @@
+Physics=80
+Chemistry=100
+Math=100
+total=Physics+Chemistry+Math
+per=total*100/450
+print("the results")
+print(total)
+print("Physics - ",Physics)
+print("Chemistry - ",Chemistry)
+print("Math - ",Math)
+print("-----------------")
+print("%", per)
+print("Total Results =",total)
+print("-----------------")
