d97fe8919fab071f85e13d6af9cd4b7b5cf3ade8
engineern2019 <alidawood15@gmail.com>
2019-05-30 15:30:59 +0100

    replace

--- file_replace_letters.py
+++ file_replace_letters.py
@@ -1,8 +1,9 @@
+#program to replace one charecter with a * in the commute2.txt file.
 file_read=open("commute.txt", "r")
 file_write=open("commute2.txt", "w")
 for data in file_read:
     for ch in data:
-        if ch==" ":
-            print("*",end="")
+        if ch=="y":
+            print("*",file=file_write,end="")
         else:
-            print(ch,end+"")
+            print(ch,file=file_write,end="")
